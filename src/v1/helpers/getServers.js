0a58029c5f67b78cbb4fafed99126e4338f5f8ab
Andrew Molchanov <mr.catmanrus@gmail.com>
2020-12-29 21:34:14 +0300

    Delete getServers.js

--- src/v1/helpers/getServers.js
+++
@@ -1,8 +0,0 @@
-const Server = require("../models/Server")
-
-module.exports = async function(type) {
-    return await DB.query(
-        'SELECT servers.id, servers.price, servers.cpu, servers.ram, servers.storage, servers.traffic, '+
-        'servers.ddos, servers.geekbench, servers.geekbench_multithread, server_groups.name as group_name '+
-        'FROM servers JOIN server_groups ON servers.group_id = server_groups.id WHERE type = ?', [type])
-}
